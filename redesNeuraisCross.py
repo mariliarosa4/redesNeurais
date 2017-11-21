29482e06765eb0cc11431c3a97f69c3a48256379
marilia <mariliarosilva@gmail.com>
2017-11-21 00:05:27 -0200

    matriz de confusao para cross validation

--- redesNeuraisCross.py
+++ redesNeuraisCross.py
@@ -4,20 +4,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score, cross_val_predict,KFold
-from matplotlib import pyplot as plt
 from sklearn import metrics
 
 from sklearn.metrics import classification_report,confusion_matrix
 dataset = pd.read_csv("mammographic_masses_classficacao.data.csv",sep=",")
-dataset.iloc[1] = dataset.iloc[1].apply(pd.to_numeric, errors='coerce')
-dataset.iloc[0] = dataset.iloc[0].apply(pd.to_numeric, errors='coerce')
-dataset.iloc[2] = dataset.iloc[2].apply(pd.to_numeric, errors='coerce')
-dataset.iloc[3] = dataset.iloc[3].apply(pd.to_numeric, errors='coerce')
-dataset.iloc[4] = dataset.iloc[4].apply(pd.to_numeric, errors='coerce')
 dataset = dataset.replace("?", -1)
 X = numpy.array(dataset.iloc[:,:4])
 y = numpy.array(dataset.iloc[:,5])
-
+def somarMatrizes(matriz1, matriz2):
+    if(len(matriz1) != len(matriz2) or len(matriz1[0]) != len(matriz2[0])):
+        return None
+    result = []
+    for i in range(len(matriz1)):
+        result.append([])
+        for j in range(len(matriz1[0])):
+            result[i].append(matriz1[i][j] + matriz2[i][j])
+    return result
 seed = 7
 numpy.random.seed(seed)
 
@@ -34,26 +36,25 @@ MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
 kf = KFold(n_splits=10) # Define the split - into 2 folds
 kf.get_n_splits(X) # returns the number of splitting iterations in the cross-validator
 print(kf)
-KFold(n_splits=10, random_state=None, shuffle=False)
-
+cont=1
 for train_index, test_index in kf.split(X):
-     print("TRAIN:", train_index, "TEST:", test_index)
+
+     #print("TRAIN:", train_index, "TEST:", test_index)
      X_train, X_test = X[train_index], X[test_index]
      y_train, y_test = y[train_index], y[test_index]
 
      mlp.fit(X_train, y_train)
-     print confusion_matrix(y_test, mlp.predict(numpy.array(X_test)))
+     if (cont==1):
+        matriz = confusion_matrix(y_test, mlp.predict(numpy.array(X_test)))
+     if (cont==2):
+         matriz = somarMatrizes(confusion_matrix(y_test, mlp.predict(numpy.array(X_test))), matriz)
+         print(matriz)
+     cont = 2
+
+print(matriz)
 
 
-#
-#
-# results = cross_val_score(mlp, X, y, cv=kfold)
-# predictions = cross_val_predict(mlp, X, y, cv=kfold)
-# accuracy = metrics.r2_score(y, predictions)
-# print "Accuracy:", results.mean()
-#
-# predict = (cross_val_predict(mlp,X,y,cv=kfold))
-# conf_mat = confusion_matrix(y,predict)
-# print(conf_mat)
+results = cross_val_score(mlp, X, y, cv=kf.get_n_splits(X))
+print "Accuracy:", results.mean()
 
-# print(classification_report(y_test,predictions))
+
